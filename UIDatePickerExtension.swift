b5d7278795ee774fce8aa712ab077b5cfede5a04
Girisankar G <girisankar30@gmail.com>
2021-09-27 13:31:03 +0530

    Create UIDatePickerExtension.swift

---
+++ UIDatePickerExtension.swift
@@ -0,0 +1,14 @@
+extension UIDatePicker {
+    func set18YearValidation() {
+        let currentDate: NSDate = NSDate()
+        let calendar: NSCalendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)!
+        calendar.timeZone = NSTimeZone(name: "UTC")! as TimeZone
+        let components: NSDateComponents = NSDateComponents()
+        components.calendar = calendar as Calendar
+        components.year = -18
+        let maxDate: NSDate = calendar.date(byAdding: components as DateComponents, to: currentDate as Date, options: NSCalendar.Options(rawValue: 0))! as NSDate
+        components.year = -150
+        let minDate: NSDate = calendar.date(byAdding: components as DateComponents, to: currentDate as Date, options: NSCalendar.Options(rawValue: 0))! as NSDate
+        self.minimumDate = minDate as Date
+        self.maximumDate = maxDate as Date
+    } }
